c4b3b3b7cd3781abaf49076e0c281fa41d6e4af0
g-homebase <gabidi@gmail.com>
2019-08-20 18:30:33 -0400

    More exports

--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ export { client as otsClient } from "./clients/otsClient";
 export { cypherNodeMatrixBridge } from "./bridge/cypherNodeMatrixBridge";
 export { cypherNodeMqttSub } from "./lib/mqttUtil";
 export { getSyncMatrixClient } from "./lib/matrixUtil";
-export { crypto as cryptoUtils   } from "./lib/cryptoUtil";
+export { default as cypherNodeClient } from "./lib/cypherNodeClient";
+export { crypto as cryptoUtils } from "./lib/cryptoUtil";
 export {
   cypherNodeMatrixTransport
 } from "./transport/cyphernodeMatrixTransport";

60496a5b1fce9e1e8c5019af0e71da25a7924110
gus-t460 <gabidi@gmail.com>
2020-02-25 11:51:41 -0500

    Provided compiled JS

--- src/clients/otsClient.spec.js
+++ src/clients/otsClient.spec.js
@@ -11,10 +11,11 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -45,7 +46,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
 var crypto_1 = require("crypto");
 var ava_1 = require("ava");
@@ -56,7 +56,7 @@ test.before(function (t) {
             .update(Date.now + ":" + parseInt(Math.random() * 100))
             .digest("hex") }, otsClient_1.client());
 });
-test("Should be able to generate an OTS file", function (t) { return __awaiter(_this, void 0, void 0, function () {
+test("Should be able to generate an OTS file", function (t) { return __awaiter(void 0, void 0, void 0, function () {
     var _a, stamp, fileHash, hashRcpt;
     return __generator(this, function (_b) {
         switch (_b.label) {
@@ -72,7 +72,7 @@ test("Should be able to generate an OTS file", function (t) { return __awaiter(_
         }
     });
 }); });
-test("Should be able to Verify an OTS file hash", function (t) { return __awaiter(_this, void 0, void 0, function () {
+test("Should be able to Verify an OTS file hash", function (t) { return __awaiter(void 0, void 0, void 0, function () {
     var _a, verifyFileStamp, fileHash, _b, method, hash, result, message;
     return __generator(this, function (_c) {
         switch (_c.label) {
@@ -87,7 +87,7 @@ test("Should be able to Verify an OTS file hash", function (t) { return __awaite
         }
     });
 }); });
-test("Should be able to get OTS stamp", function (t) { return __awaiter(_this, void 0, void 0, function () {
+test("Should be able to get OTS stamp", function (t) { return __awaiter(void 0, void 0, void 0, function () {
     var _a, getStamp, fileHash, poop;
     return __generator(this, function (_b) {
         switch (_b.label) {

29f5667f2c08ba2f97bf0827b5067c29deece231
g-homebase <gabidi@gmail.com>
2020-05-05 14:22:15 -0400

    Add react-native transport and package field

---
+++ src/transport/cypherNodeHttpTransportNoAgent.ts
@@ -0,0 +1,43 @@
+import {
+  CypherNodeHTTPTransportParam,
+  CypherNodeTransport,
+  CypherNodeCommand
+} from "../lib/types/clients";
+const CypherNodeGatewayUrl =
+  (process && process.env.CYPHER_GATEWAY_URL) || "https://localhost:2009/v0/";
+const CypherNodeApiKey = (process && process.env.CYPHERNODE_API_KEY) || "";
+const CypherNodeApiKeyID = (process && process.env.CYPHERNODE_API_KEY_ID) || 3;
+const CypherNodeCertCAPem =
+  (process && process.env.CYPHERNODE_GATEKEEPER_CERT_CA) || "";
+export default ({
+  gatewayUrl = CypherNodeGatewayUrl,
+  proxyUrl = process.env.CYPHERNODE_HTTP_TRANSPORT_PROXY,
+  auth,
+  agent
+}: CypherNodeHTTPTransportParam = {}): CypherNodeTransport => {
+  if (!auth || !agent) throw "Missing authentication and or agent";
+  // Extend superagent with proxyUrl
+  const transport = {
+    async get<T>(command: CypherNodeCommand, payload?: any): Promise<T> {
+      const token = await auth();
+      const { body } = await agent
+        .get(`${gatewayUrl}${command}/${payload ? payload : ""}`)
+        .ca(CypherNodeCertCAPem)
+        .set("Authorization", `Bearer ${token}`);
+      return body;
+    },
+    async post<T>(command: CypherNodeCommand, payload: any): Promise<T> {
+      const token = await auth();
+      const { body } = await agent
+        .post(`${gatewayUrl}${command}`)
+        .ca(CypherNodeCertCAPem)
+        .set("Authorization", `Bearer ${token}`)
+        .send(payload);
+      return body;
+    }
+  };
+
+  return {
+    ...transport
+  };
+};

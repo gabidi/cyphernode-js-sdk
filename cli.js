b6fccd02677363b89ad68dcbe0a453524274c645
g-homebase <gabidi@gmail.com>
2020-05-05 08:24:11 -0400

    Make cli auto resolve promises

--- cli.js
+++ cli.js
@@ -1,10 +1,21 @@
 const { client: lnclient } = require("./src/clients/lncClient");
 const { client: btclient } = require("./src/clients/btcClient");
 const { client: wasabiClient } = require("./src/clients/wasabiClient");
+const repl=require('repl');
 
 const ln = lnclient();
 const btc = btclient();
 const wasabi = wasabiClient();
 const { log, error } = console;
 
-module.exports = { ln, btc, wasabi };
+function replEvalPromise(cmd,ctx,filename,cb) {
+  let result=eval(cmd);
+  if (result instanceof Promise) {
+    return result
+      .then(response=>cb(null,response));
+  }
+  return cb(null, result);
+}
+repl.start({ prompt: 'cyphernode-cli > ', eval: replEvalPromise });
+
+// module.exports = { ln, btc, wasabi };

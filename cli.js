262f605a7dee16f8c929c9b62b361a8ddb3b4e14
g-homebase <gabidi@gmail.com>
2020-05-05 08:26:11 -0400

    lint cli

--- cli.js
+++ cli.js
@@ -1,21 +1,18 @@
 const { client: lnclient } = require("./src/clients/lncClient");
 const { client: btclient } = require("./src/clients/btcClient");
 const { client: wasabiClient } = require("./src/clients/wasabiClient");
-const repl=require('repl');
+const repl = require("repl");
 
 const ln = lnclient();
 const btc = btclient();
 const wasabi = wasabiClient();
 const { log, error } = console;
 
-function replEvalPromise(cmd,ctx,filename,cb) {
-  let result=eval(cmd);
+const replEvalPromise = (cmd, ctx, filename, cb) => {
+  let result = eval(cmd);
   if (result instanceof Promise) {
-    return result
-      .then(response=>cb(null,response));
+    return result.then(response => cb(null, response));
   }
   return cb(null, result);
-}
-repl.start({ prompt: 'cyphernode-cli > ', eval: replEvalPromise });
-
-// module.exports = { ln, btc, wasabi };
+};
+repl.start({ prompt: "cyphernode-cli > ", eval: replEvalPromise });
